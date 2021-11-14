fc3da109462bc414cf7bf2967bb4936cfdd28a31
Altxander Kozlov <mr.kozelberg@yandex.ru>
2021-11-14 19:05:59 +0300

    dif_fr->upgrade

--- dif_fr/scripts/angles.py
+++ dif_fr/scripts/angles.py
@@ -1,10 +1,14 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-m1 = np.arange(-3,4)
-theta1 = np.arcsin(m1 * 650e-9 / 0.52e-3) / np.pi * 180 * 60
+def angles():
+    m1 = np.arange(-3,4)
+    theta1 = np.arcsin(m1 * 650e-9 / 0.52e-3) / np.pi * 180 * 60
+    
+    m2 = np.arange(-6,7)
+    theta2 = np.arcsin(m2 * 650e-9 / 1.5e-3 / 2) / np.pi * 180 * 60
+    
+    return theta1, theta2
 
-m2 = np.arange(-6,7)
-theta2 = np.arcsin(m2 * 650e-9 / 1.5e-3 / 2) / np.pi * 180 * 60
-
-print(theta1)
-print(theta2)
+if __name__ == "__main__":
+    
